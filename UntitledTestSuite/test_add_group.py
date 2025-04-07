af9edc77d4490d60a9329246f1817e9c2b028021
George <ponosov.qa@gmail.com>
2025-04-07 15:13:31 +0200

    Moved variables into separate functions

---
+++ UntitledTestSuite/test_add_group.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+import unittest
+from selenium import webdriver
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+
+class TestAddGroup(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Chrome()
+        self.wd.implicitly_wait(30)
+
+    def test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, "admin", "secret")
+        self.create_group(wd)
+        self.filling_in_group_data(wd, name="Test_test_ran_123", header="Test_test_ran_123", houther="Test_test_ran_123")
+        self.save_and_end(wd)
+
+        # Сохранение и выход
+    def save_and_end(self, wd):
+        wd.find_element(By.NAME, "submit").click()
+        wd.find_element(By.LINK_TEXT, "groups").click()
+        wd.find_element(By.LINK_TEXT, "Logout").click()
+
+    def filling_in_group_data(self, wd, name, header , houther):
+        # Заполнение данных группы
+        wd.find_element(By.NAME, "group_name").click()
+        wd.find_element(By.NAME, "group_name").clear()
+        wd.find_element(By.NAME, "group_name").send_keys(name)
+        wd.find_element(By.NAME, "group_header").click()
+        wd.find_element(By.NAME, "group_header").clear()
+        wd.find_element(By.NAME, "group_header").send_keys(header)
+        wd.find_element(By.NAME, "group_footer").click()
+        wd.find_element(By.NAME, "group_footer").clear()
+        wd.find_element(By.NAME, "group_footer").send_keys(houther)
+
+    def create_group(self, wd):
+        # Создание группы
+        wd.find_element(By.LINK_TEXT, "groups").click()
+        wd.find_element(By.NAME, "new").click()
+
+    def login(self, wd, username = "admin", password =  "secret"):
+        # Логин
+        wd.find_element(By.NAME, "user").click()
+        wd.find_element(By.NAME, "user").clear()
+        wd.find_element(By.NAME, "user").send_keys(username)
+        wd.find_element(By.NAME, "pass").click()
+        wd.find_element(By.NAME, "pass").clear()
+        wd.find_element(By.NAME, "pass").send_keys(password)
+        wd.find_element(By.XPATH, "//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        # Открытие основной страницы
+        wd.get("http://localhost/addressbook/")
+
+    def is_element_present(self, how, what):
+        try: self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.wd.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
