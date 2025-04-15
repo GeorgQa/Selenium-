0db00d5747c43f5fd477893421429499603504dd
George <ponosov.qa@gmail.com>
2025-04-15 10:05:03 +0200

    Перенес в класс BaseCase несколько вспомогательных методов

--- lib/base_case.py
+++ lib/base_case.py
@@ -13,3 +13,25 @@ class BaseCase:
         def open_home_page(wd):
             # Открытие основной страницы
             wd.get("http://localhost/addressbook/")
+
+
+        @staticmethod
+        def create_group(self, wd):
+            # Создание группы
+            wd.find_element(By.LINK_TEXT, "groups").click()
+            wd.find_element(By.NAME, "new").click()
+
+        @staticmethod
+        def filling_in_group_data(self, wd, name, header, fouther):
+            # Заполнение данных группы
+            wd.find_element(By.NAME, "group_name").send_keys(name)
+            wd.find_element(By.NAME, "group_header").send_keys(header)
+            wd.find_element(By.NAME, "group_footer").send_keys(fouther)
+
+
+        @staticmethod
+        def save_and_exit(self, wd):
+            wd.find_element(By.NAME, "submit").click()
+            wd.find_element(By.LINK_TEXT, "groups").click()
+            wd.find_element(By.LINK_TEXT, "Logout").click()
+
--- untitledTestSuite/test_add_group.py
+++ untitledTestSuite/test_add_group.py
@@ -17,44 +17,17 @@ class TestAddGroup():
         wd = self.wd
         BaseCase.open_home_page(wd)
         BaseCase.login(wd)
-        self.create_group(wd)
-        self.filling_in_group_data(wd, "new_group", "Header", "vtgtr")
-        self.save_and_end(wd)
+        BaseCase.create_group(wd)
+        BaseCase.filling_in_group_data(wd, "new_group", "Header", "vtgtr")
+        BaseCase.save_and_exit(wd)
 
     def test_no_data_group(self):
         wd = self.wd
         BaseCase.open_home_page(wd)
         BaseCase.login(wd)
-        self.create_group(wd)
-        self.filling_in_group_data(wd, " ", " ", " ")
-        self.save_and_end(wd)
-
-        # Сохранение и выход
-    def save_and_end(self, wd):
-        wd.find_element(By.NAME, "submit").click()
-        wd.find_element(By.LINK_TEXT, "groups").click()
-        wd.find_element(By.LINK_TEXT, "Logout").click()
-
-    def filling_in_group_data(self, wd, name, header, fouther):
-        # Заполнение данных группы
-        wd.find_element(By.NAME, "group_name").send_keys(name)
-        wd.find_element(By.NAME, "group_header").send_keys(header)
-        wd.find_element(By.NAME, "group_footer").send_keys(fouther)
-
-    def create_group(self, wd):
-        # Создание группы
-        wd.find_element(By.LINK_TEXT, "groups").click()
-        wd.find_element(By.NAME, "new").click()
-
-    # def login(self, wd, username = "admin", password =  "secret"):
-    #     # Логин
-    #     wd.find_element(By.NAME, "user").click()
-    #     wd.find_element(By.NAME, "user").clear()
-    #     wd.find_element(By.NAME, "user").send_keys(username)
-    #     wd.find_element(By.NAME, "pass").click()
-    #     wd.find_element(By.NAME, "pass").clear()
-    #     wd.find_element(By.NAME, "pass").send_keys(password)
-    #     wd.find_element(By.XPATH, "//input[@value='Login']").click()
+        BaseCase.create_group(wd)
+        BaseCase.filling_in_group_data(wd, " ", " ", " ")
+        BaseCase.save_and_exit(wd)
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
