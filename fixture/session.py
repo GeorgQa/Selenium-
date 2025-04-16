27279435a163679e06467ebbc248f23b3514d042
George <ponosov.qa@gmail.com>
2025-04-16 16:16:14 +0200

    Обновил функцию для авторизации

--- fixture/session.py
+++ fixture/session.py
@@ -10,9 +10,9 @@ class SessionHelper:
         wd = self.app.wd
         wd.find_element(By.LINK_TEXT, "Logout").click()
 
-    def login(self):
+    def login(self, user ="admin", password ="secret"  ):
         wd = self.app.wd
         self.app.open_home_page()
-        wd.find_element(By.NAME, "user").send_keys("admin")
-        wd.find_element(By.NAME, "pass").send_keys("secret")
+        wd.find_element(By.NAME, "user").send_keys(user)
+        wd.find_element(By.NAME, "pass").send_keys(password)
         wd.find_element(By.ID, "LoginForm").submit()
--- tests/test_del_group.py
+++ tests/test_del_group.py
@@ -1,38 +1,22 @@
 
+import pytest
 
-from selenium.webdriver.common.by import By
-from selenium import webdriver
-from selenium.common.exceptions import (NoAlertPresentException,
-                                        NoSuchElementException)
-
-from fixture.session import SessionHelper
+from fixture.applications import Applications
 
 
-class TestAddGroup():
-    def setup_method(self):
-        self.wd = webdriver.Chrome()
-        self.wd.implicitly_wait(30)
-        self.session = SessionHelper(self)
+@pytest.fixture
+def app(request):
+    fixture = Applications()
+    request.addfinalizer(fixture.destroy)
+    return  fixture
 
-    def delete_first_group(self):
-        wd = self.wd
-        self.BaseCase.open_home_page()
-        # select
-        wd.find_element(By.NAME, "selected[]").click()
-        # delete
-        wd.find_element(By.NAME, "delete").click()
-        wd.find_element(By.NAME,  "group page").click()
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
+def test_delete_group(app):
+    app.open_home_page()
+    app.session.login()
+    app.group.create()
+    app.group.filling_in_group_data( "new_group", "Header", "vtgtr")
+    app.group.save()
+    app.session.logout()
 
 
-    def tearDown(self):
-        self.BaseCase.destroy()
+
